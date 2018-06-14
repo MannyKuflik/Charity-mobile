2b2fe4cd932733d2e7128696e6b393585a415e6f
MannyKuflik <mannykuflik@gmail.com>
2018-06-14 14:16:14 +0200

    LocalStorage and authserv

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
 import { ProfilePage } from "../profile/profile";
 import { Http } from "@angular/http";
+import { AuthServ } from '../../authserv';
 
 @Component({
   selector: "page-login",
@@ -12,35 +13,43 @@ export class LoginPage {
   public email: string;
   public password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public authService: AuthServ) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad LoginPage");
   }
 
   login() {
-    this.http
-      .post("http://localhost:3000/login", {
-        email: this.email,
-        password: this.password
-      })
-      .subscribe(
-        result => {
-          console.log(result.json().firstname);
-          var user = result.json();
+    // this.http
+    //   .post("http://localhost:3000/login", {
+    //     email: this.email,
+    //     password: this.password
+    //   })
+    //   .subscribe(
+    //     result => {
+    //       console.log(result.json().firstname);
+    //       var user = result.json();
 
 
-          // Our username and password (on this) should have data from the user
-          this.navCtrl.push(ProfilePage, {
-            email: this.email,
-            password: this.password,
-            firstname: user.firstname
-          });
-        },
+    //       // Our username and password (on this) should have data from the user
+    //       this.navCtrl.push(ProfilePage, {
+    //         email: this.email,
+    //         password: this.password,
+    //         firstname: user.firstname
+    //       });
+    //     },
 
-        error => {
-          console.log(error);
-        }
-      );
+    //     error => {
+    //       console.log(error);
+    //     }
+    //   );
+    let cb = (err) => {
+      if (err) {
+        return;
+      }
+      this.navCtrl.push(ProfilePage);
+    }
+    this.authService.login(this.email, this.password, cb)
+
   }
 }
