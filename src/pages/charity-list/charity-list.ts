720a7dda5c4161df572fb29b19488f953d95398b
Connor Haynes <connorhaynes@Connors-MacBook-Pro-2.local>
2018-06-21 13:08:33 +0200

    charity list

--- src/pages/charity-list/charity-list.ts
+++ src/pages/charity-list/charity-list.ts
@@ -15,34 +15,34 @@ export class CharityListPage {
     
     var charity1 = new Charity();
     charity1.id = 1;
-    charity1.name = "Rhino Poaching";
-    charity1.description = "Save the Rhinos!";
-    charity1.link="www.savetherhinos.com";
-    charity1.logoUrl = "https://upload.wikimedia.org/wikipedia/commons/6/63/Diceros_bicornis.jpg";
+    charity1.name = "Save The Rhino";
+    charity1.description = "More than 8,000 rhinos have been killed for their horns since 2007. Together we can halt the key threats of poaching and habitat loss so that rhinos will no longer be near extinction. But we need to act now.";
+    charity1.link="https://www.savetherhino.org";
+    charity1.logoUrl = "assets/imgs/savetherhino.svg";
     // charity1.photo = "assets/imgs/rhino.png";
 
     var charity2 = new Charity();
     charity2.id = 2;
-    charity2.name = "Dog Poaching";
-    charity2.description = "Save the dogs!";
-    charity2.link="www.savethedogs.com";
-    charity2.logoUrl = "https://upload.wikimedia.org/wikipedia/commons/b/ba/African_wild_dog_%28Lycaon_pictus_pictus%29.jpg";
+    charity2.name = "Painted Dog";
+    charity2.description = "Painted Dog Conservation is an organisation entirely dedicated to conserving the endangered African wild dog aka painted dog.";
+    charity2.link="https://www.painteddog.org";
+    charity2.logoUrl = "assets/imgs/painteddog.png";
     // charity2.photo = "assets/imgs/dog.jpg";
 
     var charity3 = new Charity();
     charity3.id = 3;
-    charity3.name = "Elephant Poaching";
-    charity3.description = "Save the elephants!";
-    charity3.link="www.savetheelephants.com";
-    charity3.logoUrl = "https://img.washingtonpost.com/wp-apps/imrs.php?src=https://img.washingtonpost.com/rf/image_960w/2010-2019/WashingtonPost/2015/10/08/Production/Health/Images/03902456-UZVE.jpg&w=1484";
+    charity3.name = "Stop Ivory";
+    charity3.description = "Stop Ivory is an independent non-government organisation which aims to protect elephants and stop the ivory trade by implementing the Elephant Protection Initiative with support from Conservation International.";
+    charity3.link="http://stopivory.org";
+    charity3.logoUrl = "assets/imgs/stopivory.png";
     // charity3.photo = "assets/imgs/elephant.jpg";
 
     var charity4 = new Charity();
     charity4.id = 4;
-    charity4.name = "Lion Poaching";
-    charity4.description = "Save the lions!";
-    charity4.link="www.savethelions.com";
-    charity4.logoUrl = "https://cdn.images.express.co.uk/img/dynamic/galleries/x701/67639.jpg";
+    charity4.name = "LionAid";
+    charity4.description = "LionAid is the UK charity working globally to save lions and end the decline of wild lion populations. We carry out world leading research into lion conservation. We are at the forefront of highlighting the true plight of lion populations. The only sustainable solutions involve all stake-holders, which is why we do more than just talk about it, we are working directly with tribes-people and their leaders in Africa to put in place sustainable and effective programs to help save lions. When you support LionAid, you support lions.";
+    charity4.link="https://lionaid.org";
+    charity4.logoUrl = "assets/imgs/lionaid.png";
     // charity4.photo = "lion.png";
 
     this.charities.push(charity1);
