2ea391ec209ae533209a38895453d6575138e45f
reneeyang99 <reneeyang99@gmail.com>
2018-06-21 12:18:46 +0200

    email validation

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -9,42 +9,47 @@ import { Http } from "@angular/http";
 })
 export class RegistrationPage {
 
-  public firstname: string; 
+  public firstname: string;
   public lastname: string;
   public email: string;
   public password: string;
   cpassword: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) { }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad LoginPage");
   }
 
-    register() {
-      if (!this.password || !this.email || 
-        !this.firstname || !this.lastname ||
+  register() {
+    if (!this.password || !this.email ||
+      !this.firstname || !this.lastname ||
       !this.cpassword) {
-        alert("Please fill out ALL entries")
-      }
-      else {
+      alert("Please fill out ALL entries")
+    }
+    else {
       if (this.password == this.cpassword) {
-      this.http
-        .post("http://localhost:3000/registration", {
-          firstname: this.firstname,
-          lastname: this.lastname,
-          email: this.email,
-          password: this.password,
-        })
-        .subscribe(
-          result => {
-            console.log(result);
-  
-            this.navCtrl.push(LoginPage);
-          },
-          error => {
-            console.log(error);
-          }
-        ); 
+        if (this.email.includes("@") && this.email.includes(".")) {
+          this.http
+            .post("http://localhost:3000/registration", {
+              firstname: this.firstname,
+              lastname: this.lastname,
+              email: this.email,
+              password: this.password,
+            })
+            .subscribe(
+              result => {
+                console.log(result);
+
+                this.navCtrl.push(LoginPage);
+              },
+              error => {
+                console.log(error);
+              }
+            );
+        }
+        else {
+          alert("Email not valid!")
+        }
       }
       else {
         alert("Passwords do not match!")
